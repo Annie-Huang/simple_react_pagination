f8a9c9733c21ae73879249f24c4b25dd992d1069
Annie-Huang <annie.hn@gmail.com>
2019-07-01 21:01:18 +1000

    Create Posts.js to list 100 posts.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
+import Posts from "./components/Posts";
 
 function App() {
     const [posts, setPosts] = useState([]);
@@ -24,8 +25,9 @@ function App() {
     // console.log(posts);
 
     return (
-        <div className="container">
-            <h1>My App</h1>
+        <div className="container mt-5">
+            <h1 className="text-primary mb-3">My Blog</h1>
+            <Posts posts={posts} loading={loading} />
         </div>
     );
 }
