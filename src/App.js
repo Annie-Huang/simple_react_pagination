3bb2d59a4cd39b0737d55dc22c6465868e0bdf3c
Annie-Huang <annie.hn@gmail.com>
2019-07-01 21:24:45 +1000

    Add Pagination.js component to give the pagination list at the bottom.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 function App() {
     const [posts, setPosts] = useState([]);
@@ -29,12 +30,13 @@ function App() {
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
     const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
-
+    // totalPosts is a number, not the list of all posts.
     return (
         <div className="container mt-5">
             <h1 className="text-primary mb-3">My Blog</h1>
             {/*<Posts posts={posts} loading={loading} />*/}
             <Posts posts={currentPosts} loading={loading} />
+            <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} />
         </div>
     );
 }
